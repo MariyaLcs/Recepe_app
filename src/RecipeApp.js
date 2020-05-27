ef731ba481606406bb556a1ad1e97f74fb0707d6
Mariya <mariya.podosinova@gmail.com>
2020-05-27 17:25:51 +0100

    Added Navbar

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import Recipe from './Recipe';
-import './RecipeApp.css';
+import React, { Component } from "react";
+import Recipe from "./Recipe";
+import Navbar from "./Navbar";
+import "./RecipeApp.css";
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Recipe
           title="pasta"
-          ingredients={['flour', 'water']}
+          ingredients={["flour", "water"]}
           instructions="Mix ingredients"
           img="spaghetti.jpg"
-          />
+        />
       </div>
     );
   }
